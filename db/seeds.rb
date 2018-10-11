72b5ee595ae5c7552f16a542ffe46106e3d743cb
joarnie <johnadamarnold@gmail.com>
2018-10-11 18:29:45 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,25 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+# artist1 = Artist.create(name: "abba")
+# artist2 = Artist.create(name: "the rolling stones")
+# artist3 = Artist.create(name: "bee gees")
+# artist4 = Artist.create(name: "beatles")
+#
+# genre1 = Genre.create(name: "rock")
+# genre2 = Genre.create(name: "rap")
+#
+# song1 = Song.create(name: "hey jude", artist_id: )
+# song2 = Song.create(name: "lalala", artist_id: )
+# song3 = Song.create(name: "the super song", artist_id: )
+# song4 = Song.create(name: "dance smash hit", artist_id: )
+#
+# #
+# # song_genre1 = SongGenre.create(song1, genre1)
+# # song_genre2 = SongGenre.create(song2, genre2)
+# # song_genre3 = SongGenre.create(song1, genre2)
+# # song_genre4 = SongGenre.create(song2, genre1)
+#
+# SongGenre.create(song_id: 1, genre_id 1)
+# SongGenre.create(song_id: 2, genre_id 1)
+# SongGenre.create(song_id: 3, genre_id 1)
+# SongGenre.create(song_id: 4, genre_id 2)
